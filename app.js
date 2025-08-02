b5b5da4e2e88830aa5517eb239f9bc52c7b4321c
techMelon <snehabhardwaj035@gmail.com>
2025-08-02 23:58:06 +0530

    create first program and learned create nested elements.

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+    React.createElement("h1", {}, "Hello, React1!"),
+    React.createElement("h2", {}, "hello, React2!"),
+]),
+React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "Hello, React3!"),
+    React.createElement("h2", {}, "hello, React4!"),
+])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
